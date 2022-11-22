23b59e8474ec5778b5caeef9080e6c9efcbd708f
Colin Sullender <shiruken343@gmail.com>
2022-11-22 15:51:10 -0500

    Add automated scraping daily at 02:00 UTC

---
+++ scrape.py
@@ -0,0 +1,12 @@
+import requests
+from datetime import datetime
+
+def download_file(url, filename):
+    r = requests.get(url)
+    with open(filename, 'w') as f:
+        f.write(r.text)
+
+date = datetime.utcnow().strftime("%Y/%m/%d")
+download_file(f"https://ton.twimg.com/birdwatch-public-data/{date}/notes/notes-00000.tsv", "notes-00000.tsv")
+download_file(f"https://ton.twimg.com/birdwatch-public-data/{date}/noteRatings/ratings-00000.tsv", "ratings-00000.tsv")
+download_file(f"https://ton.twimg.com/birdwatch-public-data/{date}/noteStatusHistory/noteStatusHistory-00000.tsv", "noteStatusHistory-00000.tsv")
